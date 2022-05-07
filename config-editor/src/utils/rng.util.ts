d0a11ef223ebc2c1b1e4cfed80e9cd9758e98f47
Max-Dov <maksimdovydenko@protonmail.com>
2022-05-07 19:16:18 +0300

    config-editor: improve rng util

--- config-editor/src/utils/rng.util.ts
+++ config-editor/src/utils/rng.util.ts
@@ -6,21 +6,20 @@ const sin = Math.sin;
 const cos = Math.cos;
 
 /**
- * Random Number Generator - RNG v0
+ * Random Number Generator - RNG v0.1
  * Generates random number based on world seed, world epoch, tile coordinates and iteration on that tile.
  * @returns number between 0 and 1.
  */
 export const rng = (seed: World['seed'], epoch: World['epoch'], coordinates: Tile['coordinates'], iteration = 0) => {
-// const random = (seed, epoch, coordinates, iteration = 0) => {
     const seedMix = abs(cos(seed));
     const epochMix = abs(sin(epoch));
     const coordinatesMix = abs(cos(Number(coordinates.join('17'))));
     const iterationMix = abs(sin(iteration));
-    const allTogetherMix = seedMix + epochMix + coordinatesMix + iterationMix
-    const shaking = String(allTogetherMix).replace('.', '')
-    let reverse = '0.'
-    for (let i = shaking.length - 2; i >= 0; i--) {
-        reverse += shaking[i]
+    const shaking = String(seedMix + epochMix + coordinatesMix + iterationMix);
+    let reverse = '0.';
+    // "length - 2" is needed because "length - 1" digit happens to be very predictable, making distribution uneven
+    for (let i = shaking.length - 2; i >= 2; i--) {
+        reverse += shaking[i];
     }
-    return Number(reverse)
-}
+    return Number(reverse);
+};
