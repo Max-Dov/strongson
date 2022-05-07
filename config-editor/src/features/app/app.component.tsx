18ae5672bae2b08ff79eac76e26693a16185363c
Max-Dov <maksimdovydenko@protonmail.com>
2022-05-07 18:17:03 +0300

    clean up config editor bootstrap

---
+++ config-editor/src/features/app/app.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './app.styles.scss';
+
+export const App = () => (
+    <div className="App">
+        <h1>Strongson's Config Editor v0</h1>
+    </div>
+);
+
+export default App;
