9e7974d0923269ed8e8d8c2c40fad0b456a22836
EulerSieger <sergantsergey@gmail.com>
2022-05-20 15:10:23 +0300

    button fix

--- config-editor/src/features/app/config-editor-components/WorldConfigEditor/TilesEditor/tiles-config-editor.tsx
+++ config-editor/src/features/app/config-editor-components/WorldConfigEditor/TilesEditor/tiles-config-editor.tsx
@@ -5,55 +5,41 @@ import { TileConfig } from '../../../../../models/tile-config.model';
 
 import TileIdConfigComponent from './TilesIdInput/tiles-id-input';
 import TileMutationEditor from './TilesMutationEditor/tile-mutation-editor';
+import TileRepresentationConfigComponent from './TilesRepresentationInput/tiles-representation-input';
 import TileNameConfigComponent from './TilesNameInput/tiles-name-input';
 import TileNeighborConstraintEditor from './TilesNeighborsEditor/tiles-neighbors-config';
-import TileRepresentationConfigComponent from './TilesRepresentationInput/tiles-representation-input';
-import TileChanceToMutateConfigComponent from './TilesMutationEditor/tiles-chance-to-mutate-input';
-import TileMutationMagnitudeConfigComponent from './TilesMutationEditor/tiles-mutation-magnitude-input';
-import TileMutationMagnitudeRadiusConfigComponent from './TilesMutationEditor/tiles-mutation-magnitude-radius-input';
-import TileMutationWeightConfigComponent from './TilesMutationEditor/tiles-mutation-weight-input';
 
 const TileConfigPage = () => {
 
     const [tileConfig, setTileConfig] = useState<Partial<TileConfig>>({});
 
-    const [addNewTile, setaddNewTile] = useState([{ addNewTileService: "" }]);
+    const [addNewTileService, setaddNewTileService] = useState([{ service: "" }]);
 
-    const handleaddNewTileServiceRemove = (index: number) => {
-        const list = [...addNewTile];
-        list.splice(index, 1);
-        setaddNewTile(list);
+    const handleServiceRemove = (index: number) => {
+        const newTileConfig = [...addNewTileService];
+        newTileConfig.splice(index, 1);
+        setaddNewTileService(newTileConfig);
     };
 
-    const handleaddNewTileServiceAdd = () => {
-        setaddNewTile([...addNewTile, { addNewTileService: "" }]);
+    const handleServiceAdd = () => {
+        setaddNewTileService([...addNewTileService, { service: "" }]);
     };
-
 
     return (
         <section className="contact-form-tiles">
-
-
-
-
-
             <div className="form-field">
-                <label htmlFor="addNewTileService">add New Tile</label>
-                {addNewTile.map((singleaddNewTileService, index) => (
-                    <div key={index} className="addNewTileServices">
+                {addNewTileService.map((_singleService, index) => (
+                    <div key={index} className="services">
                         <div className="first-division">
                             <TileIdConfigComponent tileConfig={tileConfig} setTileConfig={setTileConfig} ></TileIdConfigComponent>
                             <TileNameConfigComponent tileConfig={tileConfig} setTileConfig={setTileConfig} ></TileNameConfigComponent>
                             <TileRepresentationConfigComponent tileConfig={tileConfig} setTileConfig={setTileConfig} ></TileRepresentationConfigComponent>
-                            <TileChanceToMutateConfigComponent tileConfig={tileConfig} setTileConfig={setTileConfig} ></TileChanceToMutateConfigComponent>
-                            <TileMutationMagnitudeConfigComponent tileConfig={tileConfig} setTileConfig={setTileConfig} ></TileMutationMagnitudeConfigComponent>
-                            <TileMutationMagnitudeRadiusConfigComponent tileConfig={tileConfig} setTileConfig={setTileConfig} ></TileMutationMagnitudeRadiusConfigComponent>
-                            <TileMutationWeightConfigComponent tileConfig={tileConfig} setTileConfig={setTileConfig} ></TileMutationWeightConfigComponent>
+                            <TileMutationEditor />
                             <TileNeighborConstraintEditor />
-                            {addNewTile.length - 1 === index && (
+                            {addNewTileService.length - 1 === index && (
                                 <button
                                     type="button"
-                                    onClick={handleaddNewTileServiceAdd}
+                                    onClick={handleServiceAdd}
                                     className="add-btn"
                                 >
                                     <span>Add a Tile</span>
@@ -61,10 +47,10 @@ const TileConfigPage = () => {
                             )}
                         </div>
                         <div className="second-division">
-                            {addNewTile.length !== 1 && (
+                            {addNewTileService.length !== 1 && (
                                 <button
                                     type="button"
-                                    onClick={() => handleaddNewTileServiceRemove(index)}
+                                    onClick={() => handleServiceRemove(index)}
                                     className="remove-btn"
                                 >
                                     <span>Remove</span>
