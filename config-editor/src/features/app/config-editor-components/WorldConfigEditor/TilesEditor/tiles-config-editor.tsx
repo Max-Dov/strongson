1ae0a90c689f9bba7866033adc965426cdc88700
EulerSieger <sergantsergey@gmail.com>
2022-05-20 18:47:44 +0300

    add Neighbor

--- config-editor/src/features/app/config-editor-components/WorldConfigEditor/TilesEditor/tiles-config-editor.tsx
+++ config-editor/src/features/app/config-editor-components/WorldConfigEditor/TilesEditor/tiles-config-editor.tsx
@@ -5,65 +5,63 @@ import { TileConfig } from '../../../../../models/tile-config.model';
 
 import TileIdConfigComponent from './TilesIdInput/tiles-id-input';
 import TileMutationEditor from './TilesMutationEditor/tile-mutation-editor';
-import TileRepresentationConfigComponent from './TilesRepresentationInput/tiles-representation-input';
 import TileNameConfigComponent from './TilesNameInput/tiles-name-input';
 import TileNeighborConstraintEditor from './TilesNeighborsEditor/tiles-neighbors-config';
+import TileRepresentationConfigComponent from './TilesRepresentationInput/tiles-representation-input';
 
 const TileConfigPage = () => {
 
     const [tileConfig, setTileConfig] = useState<Partial<TileConfig>>({});
 
-    const [addNewTileService, setaddNewTileService] = useState([{ service: "" }]);
+    const [addNewTileConfig, setaddNewTileConfig] = useState([{ service: "" }]);
 
     const handleServiceRemove = (index: number) => {
-        const newTileConfig = [...addNewTileService];
+        const newTileConfig = [...addNewTileConfig];
         newTileConfig.splice(index, 1);
-        setaddNewTileService(newTileConfig);
+        setaddNewTileConfig(newTileConfig);
     };
 
     const handleServiceAdd = () => {
-        setaddNewTileService([...addNewTileService, { service: "" }]);
+        setaddNewTileConfig([...addNewTileConfig, { service: "" }]);
     };
 
     return (
         <section className="contact-form-tiles">
+            <TileIdConfigComponent tileConfig={tileConfig} setTileConfig={setTileConfig} ></TileIdConfigComponent>
+            <TileNameConfigComponent tileConfig={tileConfig} setTileConfig={setTileConfig} ></TileNameConfigComponent>
+            <TileRepresentationConfigComponent tileConfig={tileConfig} setTileConfig={setTileConfig} ></TileRepresentationConfigComponent>
+            <TileMutationEditor />
             <div className="form-field">
-                {addNewTileService.map((_singleService, index) => (
+                {addNewTileConfig.map((_singleService, index) => (
                     <div key={index} className="services">
                         <div className="first-division">
-                            <TileIdConfigComponent tileConfig={tileConfig} setTileConfig={setTileConfig} ></TileIdConfigComponent>
-                            <TileNameConfigComponent tileConfig={tileConfig} setTileConfig={setTileConfig} ></TileNameConfigComponent>
-                            <TileRepresentationConfigComponent tileConfig={tileConfig} setTileConfig={setTileConfig} ></TileRepresentationConfigComponent>
-                            <TileMutationEditor />
                             <TileNeighborConstraintEditor />
-                            {addNewTileService.length - 1 === index && (
+                            {addNewTileConfig.length - 1 === index && (
                                 <button
                                     type="button"
                                     onClick={handleServiceAdd}
                                     className="add-btn"
                                 >
-                                    <span>Add a Tile</span>
+                                    <span>Add Neighbor</span>
                                 </button>
                             )}
                         </div>
                         <div className="second-division">
-                            {addNewTileService.length !== 1 && (
+                            {addNewTileConfig.length !== 1 && (
                                 <button
                                     type="button"
                                     onClick={() => handleServiceRemove(index)}
                                     className="remove-btn"
                                 >
-                                    <span>Remove</span>
+                                    <span>Remove Neighbor</span>
                                 </button>
                             )}
                         </div>
                     </div>
                 ))}
             </div>
-
         </section>
     )
-
 
 
 
--- config-editor/src/features/app/config-editor-components/WorldConfigEditor/world-config-editor.tsx
+++ config-editor/src/features/app/config-editor-components/WorldConfigEditor/world-config-editor.tsx
@@ -16,12 +16,51 @@ const ConfigPage = () => {
 
 
 
+    const [addNewTileConfig, setaddNewTileConfig] = useState([{ service: "" }]);
+
+    const handleServiceRemove = (index: number) => {
+        const newTileConfig = [...addNewTileConfig];
+        newTileConfig.splice(index, 1);
+        setaddNewTileConfig(newTileConfig);
+    };
+
+    const handleServiceAdd = () => {
+        setaddNewTileConfig([...addNewTileConfig, { service: "" }]);
+    };
 
     return (
-        <section className="contact-form">
+        <section className="contact-form-tiles">
             <IdConfigComponent worldConfig={worldConfig} setWorldConfig={setWorldConfig} />
             <GeometryConfigComponent worldConfig={worldConfig} setWorldConfig={setWorldConfig} />
-            <TileConfigPage />
+            <div className="form-field">
+                {addNewTileConfig.map((_singleService, index) => (
+                    <div key={index} className="services">
+                        <div className="first-division">
+                            <TileConfigPage />
+                            {addNewTileConfig.length - 1 === index && (
+                                <button
+                                    type="button"
+                                    onClick={handleServiceAdd}
+                                    className="add-btn"
+                                >
+                                    <span>Add a Tile</span>
+                                </button>
+                            )}
+                        </div>
+                        <div className="second-division">
+                            {addNewTileConfig.length !== 1 && (
+                                <button
+                                    type="button"
+                                    onClick={() => handleServiceRemove(index)}
+                                    className="remove-btn"
+                                >
+                                    <span>Remove Tile</span>
+                                </button>
+                            )}
+                        </div>
+                    </div>
+                ))}
+            </div>
             <ResultConfigComponent worldConfig={worldConfig} tileConfig={tileConfig} ></ResultConfigComponent>
         </section>
     )
